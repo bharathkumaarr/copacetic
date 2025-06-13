6236116a8d5c541cabc7c7b8f16a05a8d962679a
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-06-13 14:29:45 +0530

    update challenge styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Layout from './components/layouts/Layout'
 import Dashboard from './components/layouts/Dashboard'
 import Challenge from './components/layouts/Challenge'
 function App() {
-  const selectedPage = 1 //zero for welcome, one for dashboard, two is for challenge
+  const selectedPage = 2 //zero for welcome, one for dashboard, two is for challenge
 
   const pages = {
     0: <Welcome/>,
--- src/components/layouts/Challenge.jsx
+++ src/components/layouts/Challenge.jsx
@@ -1,7 +1,34 @@
+import ProgressBar from "../ProgressBar"
+
 export default function Challenge() {
+    const word = 'copacetic'
+    const definition = 'in excellent order'
     return (
-        <div>
-            Challenge
-        </div>
+        <section id="challenge">
+            <h1>{word}</h1>
+            <p>{definition}</p>
+            <div className="helper">
+                <div>
+                    {/* CONTAINS ALL THE ERROR CORRECTED VISUAL BARS */}
+                    {[...Array(definition.length).keys()].map((element, elementIdx)=>{
+                        //determine whether or not the user has typed the character they think is correct, and show red or blue depending on whether it is right or wrong
+                        return (
+                            <div key={elementIdx}></div>
+                        )
+                    })}
+                </div>
+
+                <input type="text" placeholder="Enter the definition..." />
+            </div>
+            <div className="challenge-btns">
+                <button className="card-button-secondary">
+                    <h6>Quit</h6>
+                </button>
+                <button className="card-button-primary">
+                    <h6>I forgot</h6>
+                </button>
+            </div>
+            <ProgressBar />
+        </section>
     )
 }
