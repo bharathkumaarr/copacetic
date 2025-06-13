b2daff8a0d993fce0e2879492665f1bd4c206c2a
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-06-13 13:05:00 +0530

    update dashboard, countdown, history styles

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,7 +1,20 @@
 export default function History() {
     return (
-        <div>
-            
+        <div className="card history-card">
+            <h4>History</h4>
+            <p>You have no ateempts! Press <b>Start</b> to begin⭐️</p>
+            <div className="history-container">
+                <div className="card-button-secondary">
+                    <div>
+                        <p>Started</p>
+                        <h6>March 25 2025</h6>
+                    </div>
+                    <div>
+                        <p>Streak</p>
+                        <h6>53</h6>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
